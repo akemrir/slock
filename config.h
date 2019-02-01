cccd907c1f7233a179d53e734488f4b2143ab782
szatanjl <szatan@gecc.xyz>
2019-02-01 12:04:31 +0100

    Configure slock

---
+++ config.h
@@ -0,0 +1,15 @@
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "#000000",  /* after initialization */
+	[INPUT] =  "#005577",  /* during input */
+	[FAILED] = "#CC3333",  /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* treat pressing control keys while input is cleared like a wrong
+ * password (effective only when failonclear is set) */
+static const int failonctrlkey = 0;
+
+/* time in seconds before the monitor shuts down (-1 = do not set dpms) */
+static const int dpmstime = 10;
